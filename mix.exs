6af9acdbb26865cb2820c83bf41b4dabd0a00de4
Emily Guthrie <60673117+emkguts@users.noreply.github.com>
2025-02-03 10:21:53 -0800

    Post-fork cleanup (#1)
    * Delete maintainers
    
    * Add smartrent copyright
    
    * Rename styler config to quokka config

--- mix.exs
+++ mix.exs
@@ -1,4 +1,5 @@
 # Copyright 2024 Adobe. All rights reserved.
+# Copyright 2025 SmartRent. All rights reserved.
 # This file is licensed to you under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License. You may obtain a copy
 # of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -48,7 +49,6 @@ defmodule Quokka.MixProject do
 
   defp package() do
     [
-      maintainers: ["Matt Enlow", "Greg Mefford"],
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => @url}
     ]
